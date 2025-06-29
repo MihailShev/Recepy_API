edf346956f6f5c7201d75e0cdd2b5bb83dfc505d
Oksana Molchanova <mksenia728@gmail.com>
2025-06-29 10:25:00 +0200

    create getUsers

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import authRouter from './auth.js';
+import usersRouter from './users.js';
 
 const router = new Router();
 
 router.use('/api/auth', authRouter);
 
-// router.use('/api/users');
+router.use('/api/users', usersRouter);
 
 // router.use('/api/categories');
 
