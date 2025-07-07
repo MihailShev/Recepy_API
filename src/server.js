4991c8b0efdec2db6327f00db1183348b0ec83a5
Mihail <mykh.shev4enko@gmail.com>
2025-07-07 18:00:58 +0300

    fix cors

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ export function setupServer() {
 
   app.use(
     cors({
-      origin: 'http://localhost:5173',
+      origin: ['http://localhost:5173', 'https://tasteorama-recepy.vercel.app'],
       credentials: true,
     }),
   );
